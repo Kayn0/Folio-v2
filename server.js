c503ad1e5544f7d64fdfe34bb81ebc25dae2c880
kane.maslen <kanemaslen@gmail.com>
2025-01-07 18:39:59 +1000

    updates

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = import('express')
-const app = express()
-
-// Serve all the files in '/dist' directory
-app.use(express.static('dist'))
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
